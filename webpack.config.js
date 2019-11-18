160c98aaef7d2b00e744698b28125fb82d88dc5c
narmander <narminshahin@gmail.com>
2019-11-18 12:47:49 -0500

    loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,20 @@
 const path = require("path");
 
+module.exports = {
+  mode: "development",
+  entry: "./client",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: "css-loader" },
+      { test: /\.ts$/, use: "ts-loader" }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  }
+};
